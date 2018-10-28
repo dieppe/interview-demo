0edb9fdf02194e7b71b835172161393905f11114
Clément Vollet <cvollet@gmail.com>
2018-10-28 23:29:14 +0100

    Add form using react-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Form, RadioGroup, Radio, Text } from 'react-form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form>
+          {formApi => (
+            <form>
+              <label htmlFor="title">Title</label>
+              <RadioGroup field="title">
+                <label htmlFor="radio-input-male">Mr.</label>
+                <Radio value="male" id="radio-input-male" />
+                <label htmlFor="radio-input-female">Mrs.</label>
+                <Radio value="female" id="radio-input-female" />
+              </RadioGroup>
+              <label htmlFor="firstname">First Name</label>
+              <Text field="firstname" id="firstname" />
+              <label htmlFor="lastname">Last Name</label>
+              <Text field="lastname" id="lastname" />
+              <label htmlFor="country">Country of citizenship</label>
+              <RadioGroup field="country">
+                <label htmlFor="radio-input-brazil">Brazil</label>
+                <Radio value="brazil" id="radio-input-brazil" />
+                <label htmlFor="radio-input-france">France</label>
+                <Radio value="france" id="radio-input-france" />
+              </RadioGroup>
+              <label htmlFor="passportID">Passport ID</label>
+              <Text field="passportID" id="passportID" /> {/* UPPERCASE */}
+              <label htmlFor="email">Email</label>
+              <Text field="email" id="email" />
+            </form>
+          )}
+        </Form>
       </div>
     );
   }
